133135d50752136ee67483d2071e100b28e6ef47
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-08-19 20:36:01 -0700

    Add POST request for showing plotted CIMIS data for a station; update template

--- CIMIS_Flask/weather_data.py
+++ CIMIS_Flask/weather_data.py
@@ -1,29 +1,80 @@
+import io
+import base64
+import numpy as np
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-
 from CIMIS_Flask.db import get_db
+from sklearn.linear_model import LinearRegression
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
 
 bp = Blueprint('weather_data', __name__, url_prefix='/weather')
 
+def get_plot(rows, col_value, station_id):
+    dates, vals = zip(*[[row['date'], row[f'{col_value}']] for row in rows])
+    dates, vals = np.array(dates), np.array(vals)
+    make_ordinal = np.vectorize(lambda x: x.toordinal())
+    dates_ordinal = make_ordinal(dates)
+    station_name = rows[0]['stn_name']
+
+    x = dates_ordinal.reshape(-1,1)
+    y = vals
+
+    model = LinearRegression()
+    model.fit(x,y)
+
+    linear_reg = model.predict(x)
+
+    tick_interval = max(1, len(vals) // 15)
+
+    plt.figure(figsize=(12,6))
+    plt.plot(
+            dates,
+            vals,
+            marker='.',
+            markersize=0.7,
+            linestyle='-',
+            linewidth=0.5,
+            color='b',
+            label=f'{col_value} Data'
+        )
+    plt.plot(
+            dates,
+            linear_reg,
+            linestyle='--',
+            linewidth=3,
+            color='r',
+            label='Line of best fit'
+        )
+    plt.title(f'{col_value} for station {station_id}, {station_name}')
+    plt.xlabel('Date')
+    plt.legend()
+    plt.ylabel(f'{col_value}')
+    plt.grid(True)
+    plt.xticks(
+            ticks=dates[::tick_interval],
+            labels=dates[::tick_interval],
+            rotation=45
+        )
+    plt.tight_layout()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plt.close()
+
+    return base64.b64encode(buf.getvalue()).decode('utf-8')
+
 @bp.route('/', methods=['GET', 'POST'])
 def get_weather():
-    if request.method == 'POST':
-        station_id = request.form.get('station_id')
-        col_value = request.form.get('col_val')
-
-        flash(f'Selected Station ID: {station_id}, and value: {col_value}')
-
-        return redirect(url_for('weather_data.get_weather'))
-
     db = get_db()
 
-    station_ids = [
-        station['stn_id']
-        for station
-        in db.execute('SELECT DISTINCT stn_id FROM weather_data').fetchall()
-    ]
+    station_pairs = db.execute(
+        'SELECT DISTINCT stn_id, stn_name FROM weather_data'
+    ).fetchall()
 
     exclude_vals = ['stn_id', 'stn_name', 'cimiss_region', 'date', 'jul']
     col_vals = [
@@ -33,4 +84,22 @@ def get_weather():
         if col[0] not in exclude_vals
     ]
 
-    return render_template('weather.html', station_ids=station_ids, col_vals=col_vals)
+    if request.method == 'POST':
+        station_id = request.form.get('station_id')
+        col_value = request.form.get('col_val')
+
+        rows = db.execute(
+            f'SELECT {col_value}, date, stn_name FROM weather_data WHERE stn_id == ?',
+            (int(station_id),)
+        ).fetchall()
+
+        img_base64 = get_plot(rows, col_value, station_id) 
+
+        return render_template(
+            'weather.html',
+            station_pairs=station_pairs,
+            col_vals=col_vals,
+            image=img_base64
+        )
+
+    return render_template('weather.html', station_pairs=station_pairs, col_vals=col_vals)
