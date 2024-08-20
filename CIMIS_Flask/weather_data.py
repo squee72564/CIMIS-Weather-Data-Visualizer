67e2e7ff1b73f72b9ff8680248dbd375916ef435
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-08-19 17:11:31 -0700

    Reformats

--- CIMIS_Flask/weather_data.py
+++ CIMIS_Flask/weather_data.py
@@ -18,9 +18,19 @@ def get_weather():
         return redirect(url_for('weather_data.get_weather'))
 
     db = get_db()
-    station_ids = [station['stn_id'] for station in db.execute('SELECT DISTINCT stn_id FROM weather_data').fetchall()]
+
+    station_ids = [
+        station['stn_id']
+        for station
+        in db.execute('SELECT DISTINCT stn_id FROM weather_data').fetchall()
+    ]
 
     exclude_vals = ['stn_id', 'stn_name', 'cimiss_region', 'date', 'jul']
-    col_vals = [col[0] for col in db.execute('SELECT * FROM weather_data').description if col[0] not in exclude_vals]
+    col_vals = [
+        col[0]
+        for col
+        in db.execute('SELECT * FROM weather_data').description
+        if col[0] not in exclude_vals
+    ]
 
     return render_template('weather.html', station_ids=station_ids, col_vals=col_vals)
